ca52f3e8d0570cc10db6182497256ab883fb0f9d
Kelly Pressley <kellypressley86@gmail.com>
2019-10-01 21:29:17 -0700

    added authentication code to JS

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -59,88 +59,129 @@ $(document).ready(function () {
     firebase.initializeApp(firebaseConfig);
     firebase.analytics();
 
-    // Authentication Create Account //
+    // Authentication SignIn //
 
-    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
-        
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        
-    });
+    (function () {
+        var app = document.querySelector('#app');
 
-    // Authentication Sign In //
+        app.signIn = function () {
+            var email = app.email;
+            var password = app.password;
 
-    firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
-        
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        
-    });
+            if (!email || !password) {
+                return console.log('email and password required');
+            }
 
+            firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
 
-    firebase.auth().onAuthStateChanged(function (user) {
-        if (user) {
-            // User is signed in.
-            var displayName = user.displayName;
-            var email = user.email;
-            var emailVerified = user.emailVerified;
-            var photoURL = user.photoURL;
-            var isAnonymous = user.isAnonymous;
-            var uid = user.uid;
-            var providerData = user.providerData;
-            
-        } else {
-            // User is signed out.
-            
-        }
-    });
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                console.log('signIn error', error);
+            });
+        };
+
+        // Authentication SignUp //
+
+        app.register = function () {
+            var email = app.email;
+            var password = app.password;
+
+            if (!email || !password) {
+                return console.log('emal and password required');
+            }
+
+            firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
+                console.log('register error', error);
+                if (error.code === 'auth/email-already-in-use') {
+                    var credential = firebase.auth.EmailAuthProvider.credential(email, password);
+
+                    app.signInWithGoogle()
+                        .then(function () {
+                            firebase.auth().currentUser.link(credential)
+                                .then(function (user) {
+                                    console.log('Account linking success', user);
+                                }, function (error) {
+                                    console.log('Account linking error', error);
+                                });
+                        });
+                }
+
+            });
+        };
+
+        // Authentication with Google //
+
+        app.signInWithGoogle = function () {
+
+            var provider = new firebase.auth.GoogleAuthProvider();
+            provider.addScope('profile');
+            provider.addScope('email');
+
+            return firebase.auth().signInWithPopup(provider)
+                .catch(function (error) {
+                    console.log('Google sign in error', error);
+                });
+        };
+
+        // Authentication SignOut //
+
+        app.signOut = function() {
+
+            firebase.auth().signOut();
+        };
+
+        firebase.auth().onAuthStateChanged(function (user) {
+           app.user = user;
+           console.log('user', user);
+        });
+    }) ();
 
     // Assign the reference to the database to a variable //
 
     var database = firebase.database();
 
-    $("#submit").on("click", function (event) {
+        $("#submit").on("click", function (event) {
 
-        event.preventDefault();
+            event.preventDefault();
 
-        // variables for information we want to store on our database //
-        var example = $("#random-info").val().trim();
-
-
-        database
-            .ref()
-            .push({
-                //push info from variables to database //
-                randomInfo: example,
-
-            });
+            // variables for information we want to store on our database //
+            var example = $("#random-info").val().trim();
 
 
+            database
+                .ref()
+                .push({
+                    //push info from variables to database //
+                    randomInfo: example,
 
-        database
-            .ref()
-            .on("child_added", function (snapshot) {
-
-                let snapshotValue = snapshot.val();
-
-                console.log(snapshot.val());
-
-
-                var randomInfo = $("<td>").text(snapshotValue.randomInfo);
+                });
 
 
-                let newRow = $("<tr>");
 
-                newRow.append(randomInfo);
+            database
+                .ref()
+                .on("child_added", function (snapshot) {
+
+                    let snapshotValue = snapshot.val();
+
+                    console.log(snapshot.val());
 
 
-            });
+                    var randomInfo = $("<td>").text(snapshotValue.randomInfo);
 
-        $("#whatever we want to display").append(newRow);
+
+                    let newRow = $("<tr>");
+
+                    newRow.append(randomInfo);
+
+
+                });
+
+            $("#whatever we want to display").append(newRow);
+
+        });
+
+
+
 
     });
-
-
-
-
-});
