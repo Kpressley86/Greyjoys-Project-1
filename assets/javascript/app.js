9f2027e471b0fd1f181c651eb396b00e22be6980
Kelly Pressley <kellypressley86@gmail.com>
2019-10-01 18:50:37 -0700

    updated files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -39,68 +39,106 @@ $(document).ready(function () {
             // then parse.info.from.object into a Variable to display on screen//
 
 
-            
+
         });
     };
 
- // Initialize Firebase //
+    // Initialize Firebase //
 
- var firebaseConfig = {
-    apiKey:"",
-    authDomain:"",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: ""
-};
+    var firebaseConfig = {
+        apiKey: "AIzaSyBnFFHuk1PVtDan50yKCPIMgeViWlA3rXc",
+        authDomain: "greyjoy-project1.firebaseapp.com",
+        databaseURL: "https://greyjoy-project1.firebaseio.com",
+        projectId: "greyjoy-project1",
+        storageBucket: "",
+        messagingSenderId: "492997344720",
+        appId: "1:492997344720:web:76ccd3669451188c1d4dd5",
+        measurementId: "G-B88N2JWR6L"
+    };
 
-firebase.initializeApp(firebaseConfig);
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
 
-// Assign the reference to the database to a variable //
+    // Authentication Create Account //
 
-var database = firebase.database();
-
-$("#submit").on("click", function (event) {
-
-    event.preventDefault();
-
-    // variables for information we want to store on our database //
-    var example = $("#random-info").val().trim();
-   
-
-    database
-        .ref()
-        .push({
-            //push info from variables to database //
-            randomInfo: example,
-          
-        });
-
-
+    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
         
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        
+    });
+
+    // Authentication Sign In //
+
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
+        
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        
+    });
+
+
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            // User is signed in.
+            var displayName = user.displayName;
+            var email = user.email;
+            var emailVerified = user.emailVerified;
+            var photoURL = user.photoURL;
+            var isAnonymous = user.isAnonymous;
+            var uid = user.uid;
+            var providerData = user.providerData;
+            
+        } else {
+            // User is signed out.
+            
+        }
+    });
+
+    // Assign the reference to the database to a variable //
+
+    var database = firebase.database();
+
+    $("#submit").on("click", function (event) {
+
+        event.preventDefault();
+
+        // variables for information we want to store on our database //
+        var example = $("#random-info").val().trim();
+
+
         database
-        .ref()
-        .on("child_added", function (snapshot) {
-            
-            let snapshotValue = snapshot.val();
-            
-            console.log(snapshot.val());
-            
-    
-            var  randomInfo = $("<td>").text(snapshotValue.randomInfo);
-           
-            
-            let newRow = $("<tr>");
+            .ref()
+            .push({
+                //push info from variables to database //
+                randomInfo: example,
 
-            newRow.append(randomInfo);
-            
+            });
 
-        });
 
-    $("#whatever we want to display").append(newRow);
 
-});
+        database
+            .ref()
+            .on("child_added", function (snapshot) {
+
+                let snapshotValue = snapshot.val();
+
+                console.log(snapshot.val());
+
+
+                var randomInfo = $("<td>").text(snapshotValue.randomInfo);
+
+
+                let newRow = $("<tr>");
+
+                newRow.append(randomInfo);
+
+
+            });
+
+        $("#whatever we want to display").append(newRow);
+
+    });
 
 
 
